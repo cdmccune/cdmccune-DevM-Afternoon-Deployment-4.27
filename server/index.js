cb21e9d3ef0ee7dfe092264b22d5b5e9a6863761
cdmccune <curtmccune@gmail.com>
2022-04-27 15:39:40 -0700

    added js to modify html

--- server/index.js
+++ server/index.js
@@ -12,3 +12,18 @@ const port = process.env.PORT || 4005
 app.listen(port, () =>{
     console.log(`Listening on port ${port}`)
 })
+
+const showImage = () => {
+    let images = ["https://drive.google.com/uc?export=view&id=Rey_Kd5ZaQNVsa_EdPfZlUkF5OyBYDP2",
+    "https://drive.google.com/uc?export=view&id=1z-ddVrRhiAwmqW1jvL56nacD5URdOA4S",
+    "https://drive.google.com/uc?export=view&id=1FI6JxxwhalwqP3Qm4rlQucERAtgxJUjr",
+    "https://drive.google.com/uc?export=view&id=1UFHnXwjk33sShh-fzAp9tTJUY2Zod2BI",
+    "https://drive.google.com/uc?export=view&id=1NpuvRUWjvWTvHg3vhcU4VqjWhuHdQRgk"]
+    const image = document.createElement("img")
+    image.src = images[Math.floor(Math.random()*5)]
+    document.querySelector(".pic").appendChild(image)
+}
+
+document.getElementById("pic").addEventListener('click', showImage)
+
+
