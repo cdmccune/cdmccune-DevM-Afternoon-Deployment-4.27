f25352b240955116f3c16d9be0a26209b194bfcf
cdmccune <curtmccune@gmail.com>
2022-04-27 16:29:40 -0700

    7

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ app.get('/js', (req,res) => {
     res.sendfile(path.join(__dirname, '../public/main.js'))
 })
 
+app.use(express.static("public"))
+
 const port = process.env.PORT || 4005
 
 app.listen(port, () =>{
